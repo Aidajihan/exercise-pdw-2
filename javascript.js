41c3410928f6cb34d86ccc020448e244420b58c1
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-06-18 16:00:02 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video Boboiboy");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio Boboiboy");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Boboiboy");
+});
